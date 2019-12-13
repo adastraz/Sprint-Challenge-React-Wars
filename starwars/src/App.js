e19cd3ca109800f789b625f4ecd1b458f1c907bc
adastraz <tylergosox@gmail.com>
2019-12-13 16:01:11 -0500

    odd

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  
+  const thisIsBS= [1,2,3,4,5]
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [people, setNames] = useState('')
 
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people')
+      .then(response => {
+        console.log(response)
+        setNames(response.data.results)
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  },[])
+  console.log(people.length)
+  thisIsBS.map(x => {
+    console.log(x)
+  })
+  for(let i=0;i<people.length;i++){
+    console.log(people[i])
+  }
+
+  const letsSee = () => {
+    for(let i=0;i<people.length;i++){
+      return (
+        <>{people[i].name}</>
+        )
+    }
+  }
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      {
+        letsSee()
+      }
     </div>
   );
 }
